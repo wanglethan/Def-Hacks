2f1a5832e1d23678a91e8fa64ed2973ac3ce2bb0
liangel02 <li.angel.d02@gmail.com>
2020-06-21 18:04:26 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,17 +12,72 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export default firebase;
 
-export function createClinic(name, location, officeHours, numOfPeople) {
+export function createClinic(name, location, officeHours, numOfPeople, lat, lng) {
     const clinics = firebase.database().ref('clinics');
     const clinic = {
         clinicName: name,
         clinicLocation: location,
         clinicOfficeHours: officeHours,
-        clinicNumOfPeople: numOfPeople
+        clinicNumOfPeople: numOfPeople,
+        clinicLat: lat,
+        clinicLng: lng
     }
     clinics.push(clinic);
 }
 
+export function incrPatients(name) {
+    var clinic;
+    var key;
+    var itemsRef;
+    itemsRef = firebase.database().ref('clinics');
+    itemsRef.once('value').then(function (snapshot) {
+        let clinics = snapshot.val();
+        for (let eachkey in clinics) {
+            var tmpName = clinics[eachkey].clinicName;
+            if (tmpName.localeCompare(name) === 0) {
+                key = eachkey;
+                clinic = clinics[eachkey];
+                break;
+            }
+        }
+        var clinicRef = firebase.database().ref(`/clinics/${key}`);
+        if (clinic !== undefined) {
+            var updates = {};
+            var num = parseInt(clinic.clinicNumOfPeople);
+            num++;
+            clinic.clinicNumOfPeople = num.toString();
+            clinicRef.set(clinic);
+        }
+    });
+}
+
+export function decrPatients(name) {
+    var clinic;
+    var key;
+    var itemsRef;
+    itemsRef = firebase.database().ref('clinics');
+    itemsRef.once('value').then(function (snapshot) {
+        let clinics = snapshot.val();
+        for (let eachkey in clinics) {
+            var tmpName = clinics[eachkey].clinicName;
+            if (tmpName.localeCompare(name) === 0) {
+                key = eachkey;
+                clinic = clinics[eachkey];
+                break;
+            }
+        }
+        var clinicRef = firebase.database().ref(`/clinics/${key}`);
+        if (clinic !== undefined) {
+            var updates = {};
+            var num = parseInt(clinic.clinicNumOfPeople);
+            if (num > 0) {
+                num--;
+                clinic.clinicNumOfPeople = num.toString();
+                clinicRef.set(clinic);
+            }
+        }
+    });
+}
 export function createEmployee(uid, clinicName) {
     const employeeInfo = firebase.database().ref('employeeInfo');
     const employee = {
