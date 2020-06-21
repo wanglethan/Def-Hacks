695ef3caa6e7d0e096b8a43ff8ec59b21874d784
liangel02 <li.angel.d02@gmail.com>
2020-06-21 19:57:45 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,90 @@
+import firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyBcHqxDHOMBk59sN6SOIKUf3XWUK5teNeM",
+    authDomain: "defhacks-database.firebaseapp.com",
+    databaseURL: "https://defhacks-database.firebaseio.com",
+    projectId: "defhacks-database",
+    storageBucket: "defhacks-database.appspot.com",
+    messagingSenderId: "21353571598",
+    appId: "1:21353571598:web:ff0f14409f58b9c6afe35f",
+    measurementId: "G-30LP2F5CLN"
+};
+firebase.initializeApp(firebaseConfig);
+export default firebase;
+
+export function createClinic(name, location, officeHours, numOfPeople, lat, lng) {
+    const clinics = firebase.database().ref('clinics');
+    const clinic = {
+        clinicName: name,
+        clinicLocation: location,
+        clinicOfficeHours: officeHours,
+        clinicNumOfPeople: numOfPeople,
+        clinicLat: lat,
+        clinicLng: lng
+    }
+    clinics.push(clinic);
+}
+
+export function incrPatients(name) {
+    var clinic;
+    var key;
+    var itemsRef;
+    itemsRef = firebase.database().ref('clinics');
+    itemsRef.once('value').then(function (snapshot) {
+        let clinics = snapshot.val();
+        for (let eachkey in clinics) {
+            var tmpName = clinics[eachkey].clinicName;
+            if (tmpName.localeCompare(name) === 0) {
+                key = eachkey;
+                clinic = clinics[eachkey];
+                break;
+            }
+        }
+        var clinicRef = firebase.database().ref(`/clinics/${key}`);
+        if (clinic !== undefined) {
+            var updates = {};
+            var num = parseInt(clinic.clinicNumOfPeople);
+            num++;
+            clinic.clinicNumOfPeople = num.toString();
+            clinicRef.set(clinic);
+        }
+    });
+}
+
+export function decrPatients(name) {
+    var clinic;
+    var key;
+    var itemsRef;
+    itemsRef = firebase.database().ref('clinics');
+    itemsRef.once('value').then(function (snapshot) {
+        let clinics = snapshot.val();
+        for (let eachkey in clinics) {
+            var tmpName = clinics[eachkey].clinicName;
+            if (tmpName.localeCompare(name) === 0) {
+                key = eachkey;
+                clinic = clinics[eachkey];
+                break;
+            }
+        }
+        var clinicRef = firebase.database().ref(`/clinics/${key}`);
+        if (clinic !== undefined) {
+            var updates = {};
+            var num = parseInt(clinic.clinicNumOfPeople);
+            if (num > 0) {
+                num--;
+                clinic.clinicNumOfPeople = num.toString();
+                clinicRef.set(clinic);
+            }
+        }
+    });
+}
+export function createEmployee(uid, clinicName) {
+    const employeeInfo = firebase.database().ref('employeeInfo');
+    const employee = {
+        employeeUid: uid,
+        nameOfClinic: clinicName
+    }
+    employeeInfo.push(employee);
+}
+
+createClinic('Primacy - West Mississauga Medical', '3050 Argentia Rd, Mississauga, ON L5N 8E1, Canada', '9:00am - 4:00pm', '12', '43.59726257989271', '-79.78297942010728');
